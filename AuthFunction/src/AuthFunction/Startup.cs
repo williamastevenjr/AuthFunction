84203cb7ba5a329d18d470cd7fca6dd6014b5dfd
DESKTOP-LGNNK9G\Will <williamastevensjr@gmail.com>
2020-07-04 12:57:54 -0700

    swagger ui added

--- AuthFunction/src/AuthFunction/Startup.cs
+++ AuthFunction/src/AuthFunction/Startup.cs
@@ -10,12 +10,17 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace AuthFunction
 {
     public class Startup
     {
         public const string AppS3BucketKey = "AppS3Bucket";
+
+        private const string SwaggerDocumentVersionName = "v1";
+        private static string SwaggerDocumentServiceName => $"Auth API({SwaggerDocumentVersionName})";
+
 
         public Startup(IConfiguration configuration)
         {
@@ -31,6 +36,21 @@ namespace AuthFunction
 
             // Add S3 to the ASP.NET Core dependency injection framework.
             services.AddAWSService<Amazon.S3.IAmazonS3>();
+
+            //swagger
+            //services.AddSwaggerGen(c =>
+            //c.SwaggerDoc("v1", new OpenApiInfo { Title = "Auth Functions", Version = "v1" }));
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc(
+                      SwaggerDocumentVersionName,
+                            new OpenApiInfo
+                            {
+                                Title = SwaggerDocumentServiceName,
+                                Version = $"{SwaggerDocumentVersionName}"
+                            });
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
@@ -51,6 +71,17 @@ namespace AuthFunction
             {
                 endpoints.MapControllers();
             });
+
+            //use swagger
+            var API_PREFIX = "test";//todo
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint($"{(env.IsDevelopment() ? "" : API_PREFIX)}/swagger/{SwaggerDocumentVersionName}/swagger.json", SwaggerDocumentServiceName);
+                //c.SwaggerEndpoint(Configuration.GetSection("Swagger")["url"] + "/swagger/v1/swagger.json", "AWS Serverless Asp.Net Core Web API");
+                c.RoutePrefix = "swagger/ui";
+            });
+
         }
     }
 }
